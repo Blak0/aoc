4f7802978409ad50b707e3fbea177c2a2b4c607d
Blak0 <oblak7@onet.pl>
2022-06-04 16:01:13 +0200

    Refactor day 1

--- src/day_1_ex_2.rs
+++ src/day_1_ex_2.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::Iterator;
@@ -20,34 +21,27 @@ pub fn solution() {
 
     let lines_vec: Vec<_> = reader.lines().map(|x| x.unwrap()).collect();
 
-    let values = lines_vec
-        .iter()
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect_vec();
+    let values: Vec<i32> = lines_vec.iter().map(|x| x.parse().unwrap()).collect_vec();
 
     let sums = values
-        .into_iter()
-        .tuple_windows::<(i32, i32, i32)>()
+        .iter()
+        .tuple_windows::<(_, _, _)>()
         .map(|(a, b, c)| a + b + c)
         .collect_vec()
         .into_iter();
 
     let sum_measurments = sums
-        .tuple_windows::<(i32, i32)>()
-        .map(|(a, b)| {
-            if a < b {
-                Measurement::Increased
-            } else if a > b {
-                Measurement::Decreased
-            } else {
-                Measurement::NoChange
-            }
+        .tuple_windows::<(_, _)>()
+        .map(|(a, b)| match a.cmp(&b) {
+            Ordering::Less => Measurement::Increased,
+            Ordering::Greater => Measurement::Decreased,
+            Ordering::Equal => Measurement::NoChange,
         })
         .collect_vec();
 
     let times_increased = sum_measurments
-        .iter()
-        .filter(|x| **x == Measurement::Increased)
+        .into_iter()
+        .filter(|x| *x == Measurement::Increased)
         .count();
 
     println!("{}", times_increased);
