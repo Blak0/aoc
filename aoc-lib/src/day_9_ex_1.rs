125e5413f037f20fa1c873faed4724ba2fbbac37
Blak0 <oblak7@onet.pl>
2022-09-09 14:30:36 +0200

    9_1

--- aoc-lib/src/day_9_ex_1.rs
+++ aoc-lib/src/day_9_ex_1.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
@@ -26,47 +24,48 @@ pub fn solution() {
         })
         .collect_vec();
 
-    let mut coordinates_hashmap = Vec::new();
+    let mut lowest_points = Vec::new();
 
     let n = grid.len();
     let m = grid[0].len();
 
-    for pair in (0..n).cartesian_product(0..m) {
-        coordinates_hashmap.push(pair);
-    }
-
-    let mut lowest_points = HashSet::new();
-
-    while let Some((x, y)) = coordinates_hashmap.pop() {
-        let (left_x, left_y) = (x.checked_sub(1).or(Some(0)).unwrap(), y);
-        let (right_x, right_y) = (usize::min(x + 1, n - 1), y);
-        let (top_x, top_y) = (x, y.checked_sub(1).or(Some(0)).unwrap());
-        let (bottom_x, bottom_y) = (x, usize::min(y + 1, m - 1));
-
-        let minimum_coords = vec![
-            (left_x, left_y),
-            (right_x, right_y),
-            (top_x, top_y),
-            (bottom_x, bottom_y),
-            (x, y),
-        ]
-        .into_iter()
-        .min_by(|(x1, y1), (x2, y2)| grid[*x1][*y1].cmp(&grid[*x2][*y2]))
-        .unwrap();
-
-        if minimum_coords == (x, y) {
-            lowest_points.insert(minimum_coords);
+    for (x, y) in (0..n).cartesian_product(0..m) {
+        let top = if x != 0 {
+            grid[x-1][y]
         } else {
-            coordinates_hashmap.push(minimum_coords);
+            CellType::MAX
+        };
+
+        let bottom = if x != n-1 {
+            grid[x+1][y]
+        } else {
+            CellType::MAX
+        };
+
+        let left = if y != 0 {
+            grid[x][y-1]
+        } else {
+            CellType::MAX
+        };
+        
+        let right = if y != m-1 {
+            grid[x][y+1]
+        } else {
+            CellType::MAX
+        };
+ 
+        let minimum = vec![left, right, top, bottom]
+            .into_iter()
+            .min().unwrap();
+
+        if grid[x][y] < minimum {
+            lowest_points.push((x,y));
         }
     }
 
     let sum_of_lowest_points = lowest_points
         .iter()
-        .sorted()
-        .inspect(|x| println!("{:?}", x))
         .map(|(x, y)| 1 + grid[*x][*y] as usize)
-        .inspect(|x| println!("{:?}", &x))
         .sum::<usize>();
 
     println!("{}", sum_of_lowest_points);
